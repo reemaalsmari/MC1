2f2031ecd364184092a08606ab31477b9e8361cd
reemaalsmari <alsmarireema@gmail.com>
2023-10-12 15:01:24 +0300

    //game3

--- MC1/ContentView.swift
+++ MC1/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var guess = ""
+    @State var AnswerStatus = 0
+    @State var selectedWord = ""
+    @State var rightWords = ["Rabeh", "Sager"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      
+        ZStack {
+            Color.purple.opacity(0.8).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                Text("Team1")
+                    .bold()
+                    .font(.largeTitle)
+                    Spacer()
+                
+                
+                if AnswerStatus == 0 {
+                    
+                 Text("Guess the Word")   }
+                
+                
+                if AnswerStatus == 1 {
+                    
+                    Text("Correct")
+                    Button(action: { 
+                        guess = ""
+                        AnswerStatus = 0
+                        selectedWord = rightWords.randomElement()!
+                    }) {
+                        Text("Play again")
+                    }
+                  }
+                
+                if AnswerStatus == 2 {
+                    
+                 Text("Wrong")   }
+                
+                TextField("\tEnter your guess", text: $guess)
+                Spacer()
+                
+                Button(action: { submitAnswer()}) {
+                    Text("Got it!")
+                    
+                    
+                }
+            } .padding().onAppear(perform: {
+                selectedWord = rightWords.randomElement()!
+            })
         }
-        .padding()
+    }
+    
+    func submitAnswer(){
+        
+        if guess.lowercased() == selectedWord {
+            
+            AnswerStatus = 1
+          
+           
+        } else {
+            AnswerStatus = 2
+            guess = ""
+        }
     }
 }
 
