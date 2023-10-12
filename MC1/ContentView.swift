2574b8463c79dd724493b3345da912cdf2da80c6
reemaalsmari <alsmarireema@gmail.com>
2023-10-12 12:28:20 +0300

    Initial Commit

---
+++ MC1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MC1
+//
+//  Created by Reema Alsmari on 12/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MC1/MC1App.swift
@@ -0,0 +1,17 @@
+//
+//  MC1App.swift
+//  MC1
+//
+//  Created by Reema Alsmari on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
